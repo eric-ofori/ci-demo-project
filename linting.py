8814a2710d4470a8177ffce0362bd8a50a324397
eric-ofori <100205237+eric-ofori@users.noreply.github.com>
2025-06-20 15:15:23 +0000

    herh

--- linting.py
+++ linting.py
@@ -1,16 +1,17 @@
 """Random quote generator."""
- random
+random
+
 
 def get_random_quote():
     """Return a randomly selected motivational quote."""
     quotes = [
         "Believe you can and you're halfway there.",
-  "Don’t watch the clock; do what it does. Keep going.",
+        "Don’t watch the clock; do what it does. Keep going.",
         "The future depends on what you do today.",
-  "Dream big and dare to fail.",
+        "Dream big and dare to fail.",
         "Act as if what you do makes a difference. It does."
     ]
     return random.choice(quotes)
 
-print(get_random_quote())
 
+print(get_random_quote())
