6f48d7a6e3d52ef54213a5638784f0ed12148a07
eric-ofori <100205237+eric-ofori@users.noreply.github.com>
2025-06-20 14:40:09 +0000

    work test

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 """Simple script for CI demo."""
 
+
 def greet(name):
     """Return a greeting message for the provided name."""
-    return f"Hello, {name}!"
+
+
+return f"Hello, {name}!"
 
 
 print(greet("GitHub"))
