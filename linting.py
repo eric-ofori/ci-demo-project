4d0028f196c4c417b26b324464875e2ce10ed972
eric-ofori <100205237+eric-ofori@users.noreply.github.com>
2025-06-20 14:27:49 +0000

    another CI test

---
+++ linting.py
@@ -0,0 +1,16 @@
+"""Random quote generator."""
+
+import random
+
+def get_random_quote():
+  """Return a randomly selected motivational quote."""
+  quotes = [
+        "Believe you can and you're halfway there.",
+        "Don’t watch the clock; do what it does. Keep going.",
+    "The future depends on what you do today.",
+        "Dream big and dare to fail.",
+    "Act as if what you do makes a difference. It does."
+    ]
+return random.choice(quotes)
+
+print(get_random_quote())
