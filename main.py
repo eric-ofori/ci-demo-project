9e3727a70e454be69b32223d2ac8ad8019d010c9
eric-ofori <100205237+eric-ofori@users.noreply.github.com>
2025-06-20 15:35:29 +0000

    Got it

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
-"""Simple script for CI demo."""
+"""Simple script to demonstrate Pylint compliance."""
 
 
 def greet(name):
     """Return a greeting message for the provided name."""
-
-
-return f"Hello, {name}!"
+    return f"Hello, {name}!"
 
 
 print(greet("GitHub"))
